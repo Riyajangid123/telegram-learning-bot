daeae5e4204442167fe5b359bacc8ceda96cc78e
Riya Jangid <riyajangid02@gmail.com>
2026-06-15 09:17:57 +0530

    Added Supabase credentials

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,16 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
-    db_url = os.getenv("DATABASE_URL")
-    return psycopg2.connect(db_url)
+    conn = psycopg2.connect(
+        host=os.getenv("host"),
+        user=os.getenv("user"),
+        password=os.getenv("password"),
+        dbname=os.getenv("database"),
+        port=int(os.getenv("port", 5432)),
+        sslmode="require"  
+    )
+    return conn
