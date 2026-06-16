4f5ae211a49a2f95dca38d62c26cde7d34a7d66e
Riya Jangid <riyajangid02@gmail.com>
2026-06-16 12:46:09 +0530

    update backend

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from database.models import create_tables
 from bot.telegram_bot import run_bot
 from threading import Thread
 from server import app as flask_app
+import os
 
 def run_server():
-    flask_app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Starting Flask on port {port}")
+    flask_app.run(host="0.0.0.0", port=port)
 
 def main():
     print("Setting up database...")
     create_tables()
     print("✅ Database ready!")
+
+    print("Starting health check server thread...")
 
     Thread(target=run_server, daemon=True).start()
     print("✅ Health check server running!")
