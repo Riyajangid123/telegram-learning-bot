3fb61f9ea435769de5387bfd4fc9cff35cb8c505
Riya Jangid <riyajangid02@gmail.com>
2026-07-29 13:16:56 +0530

    updated webhook

--- bot.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-print("Current folder:", os.getcwd())
-
-load_dotenv()
-
-print("Env file exists:", os.path.exists(".env"))
-print("Token:", os.getenv("TELEGRAM_BOT_TOKEN"))
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from bot.telegram_bot import run_bot
-from bot.telegram_bot import user_sessions
+import os
+
 
 if __name__ == "__main__":
-    print("🚀 Booting up Webhook System for Render...")
+    print("🚀 Starting Telegram Learning Bot...")
+
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    if not token:
+        raise ValueError("TELEGRAM_BOT_TOKEN is missing")
+
     run_bot()
